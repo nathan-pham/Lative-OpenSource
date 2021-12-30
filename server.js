b90d833f948e0171d5f868c3d99b7e25b10db9b9
Nathan Pham <nathanpham.me@gmail.com>
2021-12-30 14:49:03 -0800

    moved api route first

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.use(bodyParser.json())
-
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client/build/index.html'));
-// });
 
 app.get('/api/test', (req, res) => {
     res.json({ message: "Hello from the server!" });
-
 })
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+
 
 const port = process.env.PORT || 5000;
 
